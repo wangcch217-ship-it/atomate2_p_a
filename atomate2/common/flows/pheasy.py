a60dbe2deeba90f6ca0e7329570d93183ed4cab4
wangcch217-ship-it <wangcch217@gmail.com>
2025-11-20 13:43:11 +0800

    Update pheasy.py

--- atomate2/common/flows/pheasy.py
+++ atomate2/common/flows/pheasy.py
@@ -197,6 +197,187 @@ class BasePhononMaker(PurePhonopyMaker, ABC):
     store_force_constants: bool = True
     socket: bool = False
 
+    def make(
+        self,
+        structure: Structure,
+        prev_dir: str | Path | None = None,
+        born: list[Matrix3D] | None = None,
+        epsilon_static: Matrix3D | None = None,
+        total_dft_energy_per_formula_unit: float | None = None,
+        supercell_matrix: Matrix3D | None = None,
+    ) -> Flow:
+        """Make flow to calculate the phonon properties.
+
+        Parameters
+        ----------
+        structure : Structure
+            A pymatgen structure object. Please start with a structure
+            that is nearly fully optimized as the internal optimizers
+            have very strict settings!
+        prev_dir : str or Path or None
+            A previous calculation directory to use for copying outputs.
+        born: Matrix3D
+            Instead of recomputing Born charges and epsilon, these values can also be
+            provided manually. If born and epsilon_static are provided, the born run
+            will be skipped it can be provided in the VASP convention with information
+            for every atom in unit cell. Please be careful when converting structures
+            within in this workflow as this could lead to errors
+        epsilon_static: Matrix3D
+            The high-frequency dielectric constant to use instead of recomputing born
+            charges and epsilon. If born, epsilon_static are provided, the born run
+            will be skipped
+        total_dft_energy_per_formula_unit: float
+            It has to be given per formula unit (as a result in corresponding Doc).
+            Instead of recomputing the energy of the bulk structure every time, this
+            value can also be provided in eV. If it is provided, the static run will be
+            skipped. This energy is the typical output dft energy of the DFT workflow.
+            No conversion needed.
+        supercell_matrix: list
+            Instead of min_length, also a supercell_matrix can be given, e.g.
+            [[1.0,0.0,0.0],[0.0,1.0,0.0],[0.0,0.0,1.0]
+        """
+        use_symmetrized_structure = self.use_symmetrized_structure
+        kpath_scheme = self.kpath_scheme
+        valid_structs = (None, "primitive", "conventional")
+        if use_symmetrized_structure not in valid_structs:
+            raise ValueError(
+                f"Invalid {use_symmetrized_structure=}, use one of {valid_structs}"
+            )
+
+        if use_symmetrized_structure != "primitive" and kpath_scheme != "seekpath":
+            raise ValueError(
+                f"You can't use {kpath_scheme=} with the primitive standard "
+                "structure, please use seekpath"
+            )
+
+        valid_schemes = ("seekpath", "hinuma", "setyawan_curtarolo", "latimer_munro")
+        if kpath_scheme not in valid_schemes:
+            raise ValueError(
+                f"{kpath_scheme=} is not implemented, use one of {valid_schemes}"
+            )
+
+        if self.code is None or self.code not in SUPPORTED_CODES:
+            raise ValueError(
+                "The code variable must be passed and it must be a supported code."
+                f" Supported codes are: {SUPPORTED_CODES}"
+            )
+
+        jobs = []
+
+        # TODO: should this be after or before structural optimization as the
+        #  optimization could change the symmetry we could add a tutorial and point out
+        #  that the structure should be nearly optimized before the phonon workflow
+        if self.use_symmetrized_structure == "primitive":
+            # These structures are compatible with many
+            # of the kpath algorithms that are used for Materials Project
+            prim_job = structure_to_primitive(structure, self.symprec)
+            jobs.append(prim_job)
+            structure = prim_job.output
+        elif self.use_symmetrized_structure == "conventional":
+            # it could be beneficial to use conventional standard structures to arrive
+            # faster at supercells with right angles
+            conv_job = structure_to_conventional(structure, self.symprec)
+            jobs.append(conv_job)
+            structure = conv_job.output
+
+        optimization_run_job_dir = None
+        optimization_run_uuid = None
+
+        if self.bulk_relax_maker is not None:
+            # optionally relax the structure
+            bulk_kwargs = {}
+            if self.prev_calc_dir_argname is not None:
+                bulk_kwargs[self.prev_calc_dir_argname] = prev_dir
+            bulk = self.bulk_relax_maker.make(structure, **bulk_kwargs)
+            jobs.append(bulk)
+            structure = bulk.output.structure
+            prev_dir = bulk.output.dir_name
+            optimization_run_job_dir = bulk.output.dir_name
+            optimization_run_uuid = bulk.output.uuid
+
+        # if supercell_matrix is None, supercell size will be determined after relax
+        # maker to ensure that cell lengths are really larger than threshold
+        if supercell_matrix is None:
+            supercell_job = self.get_supercell_matrix(structure)
+            jobs.append(supercell_job)
+            supercell_matrix = supercell_job.output
+
+        # Computation of static energy
+        total_dft_energy = None
+        static_run_job_dir = None
+        static_run_uuid = None
+        if (self.static_energy_maker is not None) and (
+            total_dft_energy_per_formula_unit is None
+        ):
+            static_job_kwargs = {}
+            if self.prev_calc_dir_argname is not None:
+                static_job_kwargs[self.prev_calc_dir_argname] = prev_dir
+            static_job = self.static_energy_maker.make(
+                structure=structure, **static_job_kwargs
+            )
+            jobs.append(static_job)
+            total_dft_energy = static_job.output.output.energy
+            static_run_job_dir = static_job.output.dir_name
+            static_run_uuid = static_job.output.uuid
+            prev_dir = static_job.output.dir_name
+        elif total_dft_energy_per_formula_unit is not None:
+            # to make sure that one can reuse results from Doc
+            compute_total_energy_job = get_total_energy_per_cell(
+                total_dft_energy_per_formula_unit, structure
+            )
+            jobs.append(compute_total_energy_job)
+            total_dft_energy = compute_total_energy_job.output
+
+        # get a phonon object from phonopy
+        displacements = self.get_displacements(structure, supercell_matrix)
+        jobs.append(displacements)
+
+        # perform the phonon displacement calculations
+        displacement_calcs = self.run_displacements(
+            displacements, prev_dir, structure, supercell_matrix
+        )
+        jobs.append(displacement_calcs)
+
+        # Computation of BORN charges
+        born_run_job_dir = None
+        born_run_uuid = None
+        if self.born_maker is not None and (born is None or epsilon_static is None):
+            born_kwargs = {}
+            if self.prev_calc_dir_argname is not None:
+                born_kwargs[self.prev_calc_dir_argname] = prev_dir
+            born_job = self.born_maker.make(structure, **born_kwargs)
+            jobs.append(born_job)
+
+            # I am not happy how we currently access "born" charges
+            # This is very vasp specific code aims and forcefields
+            # do not support this at the moment, if this changes we have
+            # to update this section
+            epsilon_static = born_job.output.calcs_reversed[0].output.epsilon_static
+            born = born_job.output.calcs_reversed[0].output.outcar["born"]
+            born_run_job_dir = born_job.output.dir_name
+            born_run_uuid = born_job.output.uuid
+
+        phonon_collect = self.get_results(
+            born,
+            born_run_job_dir,
+            born_run_uuid,
+            displacement_calcs,
+            displacements,  # ← 加这一行
+            epsilon_static,
+            optimization_run_job_dir,
+            optimization_run_uuid,
+            static_run_job_dir,
+            static_run_uuid,
+            structure,
+            supercell_matrix,
+            total_dft_energy,
+        )
+
+        jobs.append(phonon_collect)
+
+        # create a flow including all jobs for a phonon computation
+        return Flow(jobs, phonon_collect.output)
+    
     def get_displacements(
         self, structure: Structure, supercell_matrix: Matrix3D
     ) -> Job | Flow:
